009b1bcfce908d3381cba3245546c90ff731ceeb
jtlee523 <44630039+jtlee523@users.noreply.github.com>
2019-01-08 00:16:44 -0500

    Added Page 1 Page 2

--- BeliefDivergence2/pages.py
+++ BeliefDivergence2/pages.py
@@ -8,7 +8,17 @@ class Task_Two(Page):
 	form_model='player'
 	form_fields=['isGray']
 	pass
-
+	
+class Task_Two_Page1(Page): #added for the two page system
+	form_model='player'
+	#form_fields=['isGray']
+	pass
+	
+class Task_Two_Page2(Page): #added for the two page system
+	form_model='player'
+	form_fields=['isGray']
+	pass
+	
 class Task_Two_Feedback(Page):
 	form_model='player'
 	form_fields=['Red_Probability_guess', 'Yellow_Probability_guess', 'Green_Probability_guess', 'Accuracy']
@@ -28,7 +38,9 @@ class Results(Page):
 
 
 page_sequence = [
-	Task_Two,
+	#Task_Two,
+	Task_Two_Page1,
+	Task_Two_Page2,
 	Task_Two_Feedback,
 	Task_Two_FollowUp,
     #Test,
